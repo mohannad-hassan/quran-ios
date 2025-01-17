141ae0bc020c17e4d71fee000a610d5303fbc91b
Mohannad A. Hassan <mohannad.hassan.90@gmail.com>
2025-01-17 09:47:48 +0200

    Implement the first draft of CreateBookmarkRequest

--- Data/BookmarkAPI/Sources/BookmarkAPIService.swift
+++ Data/BookmarkAPI/Sources/BookmarkAPIService.swift
@@ -6,15 +6,62 @@
 //
 
 import Foundation
+import AuthenticationClient
+import QuranAnnotations
+import VLogging
 
 public protocol BookmarkAPIService {
 
-    func createBookmarkRequest() async throws -> CreateBookmarkRequest
+    func createBookmarkRequest(forPageNumber pageNumber: Int, mushafID: String) async throws -> CreateBookmarkRequest?
 }
 
-final class BookmarkAPIServiceImpl: BookmarkAPIService {
+public final class BookmarkAPIServiceImpl: BookmarkAPIService {
 
-    func createBookmarkRequest() async throws -> CreateBookmarkRequest {
-        CreateBookmarkRequest()
+    private let authenticationClient: AuthenticationClient?
+
+    public init(authenticationClient: AuthenticationClient?) {
+        self.authenticationClient = authenticationClient
+    }
+
+    public func createBookmarkRequest(forPageNumber pageNumber: Int,
+                                      mushafID: String) async throws -> CreateBookmarkRequest? {
+        guard let authenticationClient else {
+            logger.info("Bookmarks APIs. No authentication client found")
+            return nil
+        }
+        guard await authenticationClient.authenticationState == .authenticated else {
+            logger.info("Bookmarks APIs. App is not authenticated.")
+            return nil
+        }
+
+        let request = CreateBookmarkRequest(
+            pageNumber: pageNumber,
+            mushafID: mushafID,
+            networkManager: .init(authenticationClient: authenticationClient,
+                                  session: URLSession.shared)
+        )
+
+        return request
+    }
+}
+
+
+class AuthenticatedNetworkManager {
+
+    private let baseURL = "https://staging-oauth2.quran.foundation/auth/v1"
+    private let authenticationClient: AuthenticationClient
+    private let session: URLSession
+
+    init(authenticationClient: AuthenticationClient, session: URLSession) {
+        self.authenticationClient = authenticationClient
+        self.session = session
+    }
+
+    func request(path: String, method: String = "GET", payload: Data? = nil) async throws -> (Data, URLResponse) {
+        var request = URLRequest(url: URL(string: baseURL + path)!)
+        request.httpBody = payload
+        request.httpMethod = method
+        request = try await authenticationClient.authenticate(request: request)
+        return try await session.data(for: request)
     }
 }
--- Data/BookmarkAPI/Sources/CreateBookmarkRequest.swift
+++ Data/BookmarkAPI/Sources/CreateBookmarkRequest.swift
@@ -6,7 +6,61 @@
 //
 
 import Foundation
+import QuranAnnotations
+
+struct BookmarkAPIJSON: Decodable {
+    let id: String
+    let createdAt: Date
+    let type: String
+    let key: Int?
+    let verseNumber: Int?
+    let group: String?
+}
+
+// TODO: Do this
+enum TODOErrors: Error {
+    case someError
+}
 
 public struct CreateBookmarkRequest {
+    struct RequestBody: Encodable {
+        let key: Int
+        let type: String = "page"
+        let mushaf: String
+    }
 
+    private let networkManager: AuthenticatedNetworkManager
+
+    private let requestBody: RequestBody
+
+    struct Response: Decodable {
+        let success: Bool
+        let bookmark: BookmarkAPIJSON
+    }
+
+    init(pageNumber: Int,
+         mushafID: String,
+         networkManager: AuthenticatedNetworkManager) {
+        self.networkManager = networkManager
+        self.requestBody = RequestBody(key: pageNumber, mushaf: mushafID)
+    }
+
+    // Simplify logic for now; return the page number.
+    // However, it will be a question to ponder. Should the APIs in general return a model,
+    // or just some primitves and leave the details of model management to the domain services?
+    //
+
+    public func execute() async throws -> Int? {
+        let encoder = JSONEncoder()
+        let requestData = try encoder.encode(requestBody)
+        let (data, response) = try await networkManager.request(path: "/bookmarks",
+                                                                method: "POST",
+                                                                payload: requestData)
+        guard let response = response as? HTTPURLResponse, response.statusCode == 200 else {
+            throw TODOErrors.someError
+        }
+
+        let responseObject = try JSONDecoder().decode(Response.self, from: data)
+        return responseObject.bookmark.key
+    }
 }
